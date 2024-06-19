61f5b3f6d628ca60aac40f83619c812e4b9bad7f
tangkeehee2002 <tangkeehee2002@gmail.com>
2024-06-19 12:32:29 +0000

    modified:   nuxt.config.ts 	modified:   package-lock.json 	modified:   package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,29 +9,12 @@ export default defineNuxtConfig({
   },
 
   modules: [//"@nuxtjs/tailwindcss",
-  "@nuxtjs/i18n", "@nuxt/image", "nuxt-icon", "@nuxtjs/seo", "@nuxt/ui", "@nuxtjs/device", "@nuxt/fonts", "nuxt-gtag", "@formkit/nuxt", "nuxt-icons", "nuxt-svgo"],
+  "@nuxtjs/i18n", "@nuxt/image", "nuxt-icon", "@nuxtjs/seo", "@nuxt/ui", "@nuxtjs/device", "@nuxt/fonts", "nuxt-gtag", "@formkit/nuxt", "nuxt-icons", "nuxt-svgo", "@nuxthub/core"],
   alias: {
     '#tailwind-config': './tailwind.config.js'
   },
   ui: {
     global: true,
     icons: ['mdi', 'simple-icons']
-  },
-  build: {
-    html: {
-      minify: {
-        collapseBooleanAttributes: true,
-        decodeEntities: true,
-        minifyCSS: true,
-        minifyJS: true,
-        processConditionalComments: true,
-        removeEmptyAttributes: true,
-        removeRedundantAttributes: true,
-        trimCustomFragments: true,
-        useShortDoctype: true,
-        preserveLineBreaks: false, // Add this line to minify to one line
-        collapseWhitespace: true, // Add this line to remove extra whitespace
-      },
-    },
-  }  
+  }
 })
